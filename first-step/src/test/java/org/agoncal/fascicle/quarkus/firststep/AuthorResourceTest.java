1a36a08f0d68a09fc32bf4cb0f42ed9c47ca052c
Antonio Goncalves <antonio.goncalves@gmail.com>
2019-11-03 22:49:23 +0100

    Format REST Assured

--- first-step/src/test/java/org/agoncal/fascicle/quarkus/firststep/AuthorResourceTest.java
+++ first-step/src/test/java/org/agoncal/fascicle/quarkus/firststep/AuthorResourceTest.java
@@ -9,27 +9,31 @@ import static org.hamcrest.CoreMatchers.is;
 @QuarkusTest
 public class AuthorResourceTest {
 
+  // @formatter:off
   @Test
   public void shouldGetAllScifiAuthors() {
     // tag::adocShouldGetAllAuthors[]
-    given()
-      .when().get("/authors")
-      .then()
-      .statusCode(200)
-      .body(is("Isaac Asimov, Ray Bradbury, Douglas Adams"));
+    given().
+    when().
+      get("/authors").
+    then().
+      statusCode(200).
+      body(is("Isaac Asimov, Ray Bradbury, Douglas Adams"));
     // end::adocShouldGetAllAuthors[]
   }
 
   @Test
   public void shouldGet1stScifiAuthor() {
     // tag::adocShouldGetAuthor[]
-    given()
-      .when().get("/authors/0")
-      .then()
-      .statusCode(200)
-      .body(is("Isaac Asimov"));
+    given().
+    when().
+      get("/authors/0").
+    then().
+      statusCode(200).
+      body(is("Isaac Asimov"));
     // end::adocShouldGetAuthor[]
   }
+  // @formatter:on
 
   @Test
   public void shouldGet2ndScifiAuthor() {
