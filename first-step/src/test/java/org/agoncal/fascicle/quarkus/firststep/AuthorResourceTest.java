c3304d0af22970fd6e62bb25e27ae0615b52e6d7
Antonio Goncalves <antonio.goncalves@gmail.com>
2019-11-04 11:12:58 +0100

    formatter:off

--- first-step/src/test/java/org/agoncal/fascicle/quarkus/firststep/AuthorResourceTest.java
+++ first-step/src/test/java/org/agoncal/fascicle/quarkus/firststep/AuthorResourceTest.java
@@ -6,41 +6,31 @@ import org.junit.jupiter.api.Test;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 
+// @formatter:off
+// tag::adocSnippet[]
 @QuarkusTest
 public class AuthorResourceTest {
 
-  // @formatter:off
   @Test
   public void shouldGetAllScifiAuthors() {
-    // tag::adocShouldGetAllAuthors[]
     given().
     when().
       get("/authors").
     then().
       statusCode(200).
+        and().
       body(is("Isaac Asimov, Ray Bradbury, Douglas Adams"));
-    // end::adocShouldGetAllAuthors[]
   }
 
   @Test
-  public void shouldGet1stScifiAuthor() {
-    // tag::adocShouldGetAuthor[]
+  public void shouldGetScifiAuthor() {
     given().
     when().
       get("/authors/0").
     then().
       statusCode(200).
+        and().
       body(is("Isaac Asimov"));
-    // end::adocShouldGetAuthor[]
-  }
-  // @formatter:on
-
-  @Test
-  public void shouldGet2ndScifiAuthor() {
-    given()
-      .when().get("/authors/1")
-      .then()
-      .statusCode(200)
-      .body(is("Ray Bradbury"));
   }
 }
+// end::adocSnippet[]
