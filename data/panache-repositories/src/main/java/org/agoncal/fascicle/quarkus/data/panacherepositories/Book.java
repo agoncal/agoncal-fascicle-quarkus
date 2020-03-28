307973afdcfb62714078782a356b1912b67c5273
Antonio Goncalves <antonio.goncalves@gmail.com>
2020-03-28 16:06:31 +0100

    refactoring

---
+++ data/panache-repositories/src/main/java/org/agoncal/fascicle/quarkus/data/panacherepositories/Book.java
@@ -0,0 +1,206 @@
+package org.agoncal.fascicle.quarkus.data.panacherepositories;
+
+import javax.persistence.CollectionTable;
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.MapKeyColumn;
+import javax.persistence.NamedQuery;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static javax.persistence.CascadeType.PERSIST;
+
+/**
+ * @author Antonio Goncalves
+ * http://www.antoniogoncalves.org
+ * --
+ */
+// tag::adocSnippet[]
+@Entity
+@NamedQuery(name = "findAllBooks", query = "SELECT b FROM Book b")
+@NamedQuery(name = "findBookH2G2", query = "SELECT b FROM Book b WHERE b.title ='H2G2'")
+public class Book extends Item {
+
+  @Column(nullable = false, unique = true)
+  private String isbn;
+  @Column(name = "nb_Of_pages")
+  private Integer nbOfPages;
+  private Boolean illustrations;
+
+  @ElementCollection
+  @CollectionTable(name = "tags",
+    joinColumns = {@JoinColumn(name = "book_fk")}
+  )
+  @Column(name = "value")
+  private List<String> tags = new ArrayList<>();
+
+  @ElementCollection
+  @CollectionTable(name = "book_chapters",
+    joinColumns = {@JoinColumn(name = "book_fk")}
+  )
+  @MapKeyColumn(name = "position")
+  private Map<Integer, Chapter> chapters = new HashMap<>();
+
+  @ManyToMany(cascade = PERSIST)
+  @JoinTable(name = "books_authors",
+    joinColumns = {@JoinColumn(name = "book_fk")},
+    inverseJoinColumns = {@JoinColumn(name = "author_fk")}
+  )
+  private List<Author> authors = new ArrayList<>();
+
+  // Constructors, getters, setters
+  // tag::adocSkip[]
+
+  // ======================================
+  // =          Getters & Setters         =
+  // ======================================
+
+  public Long getId() {
+    return id;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public Book title(String title) {
+    this.title = title;
+    return this;
+  }
+
+  public Float getPrice() {
+    return price;
+  }
+
+  public void setPrice(Float price) {
+    this.price = price;
+  }
+
+  public Book price(Float price) {
+    this.price = price;
+    return this;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public Book description(String description) {
+    this.description = description;
+    return this;
+  }
+
+  public String getIsbn() {
+    return isbn;
+  }
+
+  public void setIsbn(String isbn) {
+    this.isbn = isbn;
+  }
+
+  public Book isbn(String isbn) {
+    this.isbn = isbn;
+    return this;
+  }
+
+  public Integer getNbOfPages() {
+    return nbOfPages;
+  }
+
+  public void setNbOfPages(Integer nbOfPages) {
+    this.nbOfPages = nbOfPages;
+  }
+
+  public Book nbOfPages(Integer nbOfPages) {
+    this.nbOfPages = nbOfPages;
+    return this;
+  }
+
+  public Boolean getIllustrations() {
+    return illustrations;
+  }
+
+  public void setIllustrations(Boolean illustrations) {
+    this.illustrations = illustrations;
+  }
+
+  public Book illustrations(Boolean illustrations) {
+    this.illustrations = illustrations;
+    return this;
+  }
+
+  public List<String> getTags() {
+    return tags;
+  }
+
+  public void setTags(List<String> tags) {
+    this.tags = tags;
+  }
+
+  public Book tag(String tag) {
+    this.tags.add(tag);
+    return this;
+  }
+
+  public Map<Integer, Chapter> getChapters() {
+    return chapters;
+  }
+
+  public void setChapters(Map<Integer, Chapter> chapters) {
+    this.chapters = chapters;
+  }
+
+  public Book chapter(Integer position, Chapter chapter) {
+    this.chapters.put(position, chapter);
+    return this;
+  }
+
+  public List<Author> getAuthors() {
+    return authors;
+  }
+
+  public void setAuthors(List<Author> authors) {
+    this.authors = authors;
+  }
+
+  public Book author(Author author) {
+    this.authors.add(author);
+    return this;
+  }
+
+  // ======================================
+  // =         hash, equals, toString     =
+  // ======================================
+
+  @Override
+  public String toString() {
+    return "Book{" +
+      "id=" + id +
+      ", title='" + title + '\'' +
+      ", description='" + description + '\'' +
+      ", price=" + price +
+      ", isbn='" + isbn + '\'' +
+      ", nbOfPages=" + nbOfPages +
+      ", illustrations=" + illustrations +
+      ", tags=" + tags +
+      ", chapters=" + chapters +
+      ", authors=" + authors +
+      '}';
+  }
+  // end::adocSkip[]
+}
+// end::adocSnippet[]
---
+++ http/json/src/main/java/org/agoncal/fascicle/quarkus/http/json/CustomerResource.java
@@ -0,0 +1,55 @@
+package org.agoncal.fascicle.quarkus.http.json;
+
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.time.LocalDate;
+
+/**
+ * @author Antonio Goncalves
+ * http://www.antoniogoncalves.org
+ * --
+ */
+@Path("/customers")
+@Produces(MediaType.APPLICATION_JSON)
+public class CustomerResource {
+
+  // ======================================
+  // =           Public Methods           =
+  // ======================================
+
+  @GET
+  @Path("/jsonb")
+  public Response getJSonB() {
+    Customer customer = new Customer().firstName("Antonio").lastName("Goncalves").email("agoncal.fascicle@gmail.com ").phoneNumber("+33 123 456").dateOfBirth(LocalDate.now());
+    return Response.ok(customer).build();
+  }
+
+  @Path("/jsonp")
+  // tag::adocSnippet[]
+  @GET
+  public Response getJSonP() {
+    JsonObject customer = Json.createObjectBuilder()
+      .add("firstName", "Antonio")
+      .add("lastName", "Goncalves")
+      .add("email", "agoncal.fascicle@gmail.com")
+      .add("address", Json.createObjectBuilder()
+        .add("street", "21 Ritherdon Rd")
+        .add("city", "Brighton")
+        .add("country", "UK"))
+      .add("phoneNumbers", Json.createArrayBuilder()
+        .add(Json.createObjectBuilder()
+          .add("type", "mobile")
+          .add("number", "+33 123 456"))
+        .add(Json.createObjectBuilder()
+          .add("type", "home")
+          .add("number", "+33 646 555")))
+      .build();
+    return Response.ok(customer).build();
+  }
+  // end::adocSnippet[]
+}
