cf08706fe8b2d4b1dd9ecdd26f5c29858aa7d7a6
Antonio Goncalves <antonio.goncalves@gmail.com>
2020-04-02 14:36:16 +0200

    Configuration

--- core/configuration-quarkus/src/main/java/org/agoncal/fascicle/quarkus/core/configuration/quarkus/ConfigurationResource.java
+++ core/configuration-quarkus/src/main/java/org/agoncal/fascicle/quarkus/core/configuration/quarkus/ConfigurationResource.java
@@ -19,6 +19,12 @@ public class ConfigurationResource {
     LOGGER.info("quarkus.application.version: " + config.getValue("quarkus.application.version", String.class));
     LOGGER.info("quarkus.banner.enabled: " + config.getValue("quarkus.banner.enabled", Boolean.class));
     LOGGER.info("quarkus.default-locale: " + config.getValue("quarkus.default-locale", String.class));
+    LOGGER.info("quarkus.http.port: " + config.getValue("quarkus.http.port", String.class));
+    LOGGER.info("quarkus.http.test-port: " + config.getValue("quarkus.http.test-port", String.class));
+    LOGGER.info("quarkus.http.ssl.protocols: " + config.getValue("quarkus.http.ssl.protocols", String.class));
+    LOGGER.info("quarkus.http.read-timeout: " + config.getValue("quarkus.http.read-timeout", String.class));
+    LOGGER.info("quarkus.log.level: " + config.getValue("quarkus.log.level", String.class));
+    LOGGER.info("quarkus.log.min-level: " + config.getValue("quarkus.log.min-level", String.class));
     // LOGGER.info("quarkus.banner.path: " + config.getValue("quarkus.banner.path", String.class));
     // LOGGER.info("quarkus.log.level: " + config.getValue("quarkus.log.level", String.class));
     // LOGGER.info("quarkus.servlet.default-charset: " + config.getValue("quarkus.servlet.default-charset", String.class));
@@ -28,5 +34,21 @@ public class ConfigurationResource {
     // LOGGER.info("quarkus.package.type: " + config.getValue("quarkus.package.type", String.class));
     // LOGGER.info("quarkus.native.java-home: " + config.getValue("quarkus.native.java-home", String.class));
     // LOGGER.info("quarkus.live-reload.url: " + config.getValue("quarkus.live-reload.url", String.class));
+  }
+
+  @Path("/test")
+  @GET
+  public void displayConfigurationInTest() {
+    // tag::adocSnippet[]
+    Config config = ConfigProvider.getConfig();
+    LOGGER.info(config.getValue("quarkus.banner.enabled", Boolean.class));
+    LOGGER.info(config.getValue("quarkus.default-locale", String.class));
+    LOGGER.info(config.getValue("quarkus.http.port", String.class));
+    LOGGER.info(config.getValue("quarkus.http.test-port", String.class));
+    LOGGER.info(config.getValue("quarkus.http.ssl.protocols", String.class));
+    LOGGER.info(config.getValue("quarkus.http.read-timeout", String.class));
+    LOGGER.info(config.getValue("quarkus.log.level", String.class));
+    LOGGER.info(config.getValue("quarkus.log.min-level", String.class));
+    // tag::adocSnippet[]
   }
 }
