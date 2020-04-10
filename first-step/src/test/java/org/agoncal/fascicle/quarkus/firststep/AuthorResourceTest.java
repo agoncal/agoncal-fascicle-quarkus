0aa0e8362f39eaedca0a8ff99a7af369ab1da649
Antonio Goncalves <antonio.goncalves@gmail.com>
2020-04-10 21:48:59 +0200

    Refactor

--- first-step/src/test/java/org/agoncal/fascicle/quarkus/firststep/AuthorResourceTest.java
+++ first-step/src/test/java/org/agoncal/fascicle/quarkus/firststep/AuthorResourceTest.java
@@ -1,38 +1,46 @@
 package org.agoncal.fascicle.quarkus.firststep;
 
 import io.quarkus.test.junit.QuarkusTest;
+import org.hamcrest.core.Is;
 import org.junit.jupiter.api.Test;
 
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.is;
+import static io.restassured.RestAssured.get;
 
+/**
+ * @author Antonio Goncalves
+ * http://www.antoniogoncalves.org
+ * --
+ */
 // @formatter:off
-// tag::adocSnippet[]
 @QuarkusTest
 public class AuthorResourceTest {
 
   @Test
-  public void shouldGetAllScifiAuthors() {
-    given().
-    when().
-      get("/authors").
-    then().
-      assertThat().
-        statusCode(is(200)).
-      and().
-        body(is("Isaac Asimov, Ray Bradbury, Douglas Adams"));
+  public void shouldGetAllAuthors() {
+    // tag::adocShouldGetAllAuthors[]
+    get("/authors").
+    then()
+      .statusCode(200)
+      .body(Is.is("Isaac Asimov, Ray Bradbury, Douglas Adams"));
+    // end::adocShouldGetAllAuthors[]
   }
 
   @Test
-  public void shouldGetScifiAuthor() {
-    given().
-    when().
-      get("/authors/0").
-    then().
-      assertThat().
-        statusCode(is(200)).
-      and().
-        body(is("Isaac Asimov"));
+  public void shouldGetAnAuthor() {
+    // tag::adocShouldGetAuthor[]
+    get("/authors/0").
+    then()
+      .statusCode(200)
+      .body(Is.is("Isaac Asimov"));
+    // end::adocShouldGetAuthor[]
+  }
+
+  @Test
+  public void shouldNotFindAnAuthor() {
+    // tag::adocShouldNotFindAnAuthor[]
+    get("/dummy").
+    then()
+      .statusCode(404);
+    // end::adocShouldNotFindResource[]
   }
 }
-// end::adocSnippet[]
