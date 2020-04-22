d1f22e02f4e27d9b1ef1c0efe4ee6bd667f79af7
Antonio Goncalves <antonio.goncalves@gmail.com>
2020-04-22 21:21:24 +0200

    Adoc

--- putting-together/rest-number/src/main/java/org/agoncal/fascicle/quarkus/number/NumberResource.java
+++ putting-together/rest-number/src/main/java/org/agoncal/fascicle/quarkus/number/NumberResource.java
@@ -15,26 +15,33 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.logging.Logger;
 
+// tag::adocSnippet[]
 @Path("/api/numbers")
 @Produces(MediaType.APPLICATION_JSON)
 public class NumberResource {
 
   private final static Logger LOGGER = Logger.getLogger(NumberResource.class.getName());
 
+  // tag::adocConfig[]
   @ConfigProperty(name = "number.separator", defaultValue = "false")
   boolean separator;
 
+  // end::adocConfig[]
   @GET
+  // tag::adocOpenAPI[]
   @Operation(summary = "Generates ISBN numbers", description = "These ISBN numbers have several formats: ISBN 13 and ISBN 10")
   @APIResponse(responseCode = "200", content = @Content(mediaType = MediaType.APPLICATION_JSON, schema = @Schema(implementation = IsbnNumbers.class)))
+  // end::adocOpenAPI[]
+  // tag::adocMetrics[]
   @Counted(name = "generateIsbnNumbers", description = "Counts how many times the generateIsbnNumbers method has been invoked")
+  // end::adocMetrics[]
   public Response generateIsbnNumbers() {
     Faker faker = new Faker();
     IsbnNumbers isbnNumbers = new IsbnNumbers();
     isbnNumbers.setIsbn10(faker.code().isbn10(separator));
     isbnNumbers.setIsbn13(faker.code().isbn13(separator));
-    LOGGER.info("ISBN book numbers generated " + isbnNumbers);
-    //return isbnNumbers;
+    LOGGER.info("ISBN numbers generated " + isbnNumbers);
     return Response.ok(isbnNumbers).build();
   }
 }
+// end::adocSnippet[]
