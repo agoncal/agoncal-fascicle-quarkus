416a3b113c782d77bf5d360d8b1e64fa1893a8fb
Antonio Goncalves <antonio.goncalves@gmail.com>
2020-04-23 16:43:49 +0200

    Adoc

--- putting-together/rest-book/src/main/java/org/agoncal/fascicle/quarkus/book/BookResource.java
+++ putting-together/rest-book/src/main/java/org/agoncal/fascicle/quarkus/book/BookResource.java
@@ -8,7 +8,6 @@ import org.eclipse.microprofile.rest.client.inject.RestClient;
 import javax.inject.Inject;
 import javax.json.Json;
 import javax.json.JsonObject;
-import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -17,19 +16,23 @@ import javax.ws.rs.core.Response;
 import java.time.LocalDateTime;
 import java.util.logging.Logger;
 
+// tag::adocSnippet[]
 @Path("/api/books")
 @Produces(MediaType.APPLICATION_JSON)
-@Consumes(MediaType.APPLICATION_JSON)
 public class BookResource {
 
   private final static Logger LOGGER = Logger.getLogger("BookResource");
 
+  // tag::adocRestClient[]
   @Inject
   @RestClient
   IsbnNumbersService isbnNumbersService;
 
+  // end::adocRestClient[]
   @GET
+  // tag::adocFaultTolerance[]
   @Fallback(fallbackMethod = "fallbackGetRandomBook")
+  // end::adocFaultTolerance[]
   // tag::adocMetrics[]
   @Counted(name = "generateIsbnNumbers", description = "Counts how many times the generateIsbnNumbers method has been invoked")
   // end::adocMetrics[]
@@ -51,6 +54,7 @@ public class BookResource {
     LOGGER.info("Random book " + book);
     return Response.ok(book).build();
   }
+  // tag::adocFaultTolerance[]
 
   private Response fallbackGetRandomBook() {
     LOGGER.warning("Falling back on creating a book");
@@ -60,4 +64,6 @@ public class BookResource {
       .build();
     return Response.ok(dummyBook).build();
   }
+  // end::adocFaultTolerance[]
 }
+// end::adocSnippet[]
