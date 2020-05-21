58e1caa89a6c93fef5197b6f67ed1af0ab0a6aa7
Antonio Goncalves <antonio.goncalves@gmail.com>
2020-05-21 16:05:37 +0200

    Getting Started

---
+++ http/jsonb-default/src/main/java/org/agoncal/fascicle/quarkus/http/jsonb/dflt/CreditCard.java
@@ -0,0 +1,75 @@
+package org.agoncal.fascicle.quarkus.http.jsonb.dflt;
+
+import javax.json.bind.annotation.JsonbProperty;
+
+/**
+ * @author Antonio Goncalves
+ * http://www.antoniogoncalves.org
+ * --
+ */
+public class CreditCard {
+
+  private String number;
+  @JsonbProperty("expiry_date")
+  private String expiryDate;
+  @JsonbProperty("control_number")
+  private Integer controlNumber;
+  @JsonbProperty("type")
+  private CreditCardType creditCardType;
+
+  // ======================================
+  // =          Getters & Setters         =
+  // ======================================
+
+  public String getNumber() {
+    return number;
+  }
+
+  public void setNumber(String number) {
+    this.number = number;
+  }
+
+  public CreditCard number(String number) {
+    this.number = number;
+    return this;
+  }
+
+  public String getExpiryDate() {
+    return expiryDate;
+  }
+
+  public void setExpiryDate(String expiryDate) {
+    this.expiryDate = expiryDate;
+  }
+
+  public CreditCard expiryDate(String expiryDate) {
+    this.expiryDate = expiryDate;
+    return this;
+  }
+
+  public Integer getControlNumber() {
+    return controlNumber;
+  }
+
+  public void setControlNumber(Integer controlNumber) {
+    this.controlNumber = controlNumber;
+  }
+
+  public CreditCard controlNumber(Integer controlNumber) {
+    this.controlNumber = controlNumber;
+    return this;
+  }
+
+  public CreditCardType getType() {
+    return creditCardType;
+  }
+
+  public void setType(CreditCardType creditCardType) {
+    this.creditCardType = creditCardType;
+  }
+
+  public CreditCard creditCardType(CreditCardType creditCardType) {
+    this.creditCardType = creditCardType;
+    return this;
+  }
+}
---
+++ http/jsonb-default/src/main/java/org/agoncal/fascicle/quarkus/http/jsonb/dflt/Customer.java
@@ -0,0 +1,75 @@
+package org.agoncal.fascicle.quarkus.http.jsonb.dflt;
+
+import javax.json.bind.annotation.JsonbProperty;
+
+/**
+ * @author Antonio Goncalves
+ * http://www.antoniogoncalves.org
+ * --
+ */
+public class Customer {
+
+  @JsonbProperty("first_name")
+  private String firstName;
+  @JsonbProperty("last_name")
+  private String lastName;
+  private String email;
+  @JsonbProperty("phone_number")
+  private String phoneNumber;
+
+  // ======================================
+  // =          Getters & Setters         =
+  // ======================================
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public Customer firstName(String firstName) {
+    this.firstName = firstName;
+    return this;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public Customer lastName(String lastName) {
+    this.lastName = lastName;
+    return this;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public Customer email(String email) {
+    this.email = email;
+    return this;
+  }
+
+  public String getPhoneNumber() {
+    return phoneNumber;
+  }
+
+  public void setPhoneNumber(String phoneNumber) {
+    this.phoneNumber = phoneNumber;
+  }
+
+  public Customer phoneNumber(String phoneNumber) {
+    this.phoneNumber = phoneNumber;
+    return this;
+  }
+}
