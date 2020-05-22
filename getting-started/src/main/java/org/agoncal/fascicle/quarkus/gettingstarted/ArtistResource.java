eb221028c9ed3fc23d763806141bea0bc892b52c
Antonio Goncalves <antonio.goncalves@gmail.com>
2020-05-22 15:49:14 +0200

    Simplify

--- getting-started/src/main/java/org/agoncal/fascicle/quarkus/gettingstarted/ArtistResource.java
+++ getting-started/src/main/java/org/agoncal/fascicle/quarkus/gettingstarted/ArtistResource.java
@@ -1,17 +1,10 @@
 package org.agoncal.fascicle.quarkus.gettingstarted;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
-import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import javax.ws.rs.core.UriInfo;
-import java.net.URI;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.UUID;
@@ -24,9 +17,6 @@ import java.util.UUID;
 // tag::adocSnippet[]
 @Path("/artists")
 @Produces(MediaType.APPLICATION_JSON)
-// tag::adocSkip[]
-@Consumes(MediaType.APPLICATION_JSON)
-// end::adocSkip[]
 public class ArtistResource {
 
   private static ArrayList<Artist> artists = new ArrayList<>(Arrays.asList(
@@ -63,45 +53,5 @@ public class ArtistResource {
   public Integer countArtists() {
     return artists.size();
   }
-
-  // tag::adocSkip[]
-  /**
-   * curl http://localhost:8080/cdbookstore/artists/e3d65ee3-7580-4dc1-b975-250cf7b8a456
-   * curl http://localhost:8080/cdbookstore/artists/e3d65ee3-7580-4dc1-b975-250cf7b8a456 | jq
-   * curl http://localhost:8080/cdbookstore/artists/e3d65ee3-7580-4dc1-b975-250cf7b8a456 -v
-   * curl -X GET http://localhost:8080/cdbookstore/artists/e3d65ee3-7580-4dc1-b975-250cf7b8a456
-   */
-  @GET
-  @Path("/{id}")
-  public Response getArtist(@PathParam("id") UUID id) {
-    Artist artist = artists.stream()
-      .filter(a -> id.equals(a.getId()))
-      .findFirst()
-      .orElse(null);
-    return Response.ok(artist).build();
-  }
-
-  /**
-   * curl -d '{"id":"8b3445fe-a0f8-4189-b39c-4c35dcd685b2", "firstName":"George", "lastName":"Martin"}' -X POST -H "Content-Type: application/json" http://localhost:8080/cdbookstore/artists -v
-   */
-  @POST
-  public Response createArtist(@Context UriInfo uriInfo, Artist artist) {
-    artist.setId(UUID.randomUUID());
-    artists.add(artist);
-    URI uri = uriInfo.getAbsolutePathBuilder().path(artist.getId().toString()).build();
-    return Response.created(uri).build();
-  }
-
-  /**
-   * curl -X DELETE http://localhost:8080/cdbookstore/artists/e3d65ee3-7580-4dc1-b975-250cf7b8a456
-   * curl -X DELETE http://localhost:8080/cdbookstore/artists/e3d65ee3-7580-4dc1-b975-250cf7b8a456 -v
-   */
-  @DELETE
-  @Path("/{id}")
-  public Response deleteArtist(@PathParam("id") UUID id) {
-    artists.remove((new Artist().id(id)));
-    return Response.noContent().build();
-  }
-  // end::adocSkip[]
 }
 // end::adocSnippet[]
--- getting-started/src/test/java/org/agoncal/fascicle/quarkus/gettingstarted/ArtistResourceTest.java
+++ getting-started/src/test/java/org/agoncal/fascicle/quarkus/gettingstarted/ArtistResourceTest.java
@@ -1,17 +1,9 @@
 package org.agoncal.fascicle.quarkus.gettingstarted;
 
 import io.quarkus.test.junit.QuarkusTest;
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
-
-import javax.ws.rs.core.MediaType;
-
-import java.util.UUID;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.is;
 
 /**
@@ -20,16 +12,11 @@ import static org.hamcrest.CoreMatchers.is;
  * --
  */
 
-// @formatter:off
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 // tag::adocSnippet[]
 @QuarkusTest
 public class ArtistResourceTest {
 
   @Test
-  // tag::adocSkip[]
-  @Order(1)
-  // end::adocSkip[]
   public void shouldGetAllArtists() {
     given().
     when().
@@ -42,27 +29,6 @@ public class ArtistResourceTest {
   }
 
   @Test
-  // tag::adocSkip[]
-  @Order(2)
-  // end::adocSkip[]
-  public void shouldGetArtist() {
-    UUID id = given().get("/artists").then().extract().response().jsonPath().getUUID("id[0]");
-
-    given().
-      pathParam("id", id).
-    when().
-      get("/artists/{id}").
-    then().
-      assertThat().
-        statusCode(is(200)).
-      and().
-        body(containsString("John"));
-  }
-
-  @Test
-  // tag::adocSkip[]
-  @Order(3)
-  // end::adocSkip[]
   public void shouldCountArtist() {
     given().
     when().
@@ -73,61 +39,5 @@ public class ArtistResourceTest {
       and().
         body(is("4"));
   }
-  // tag::adocSkip[]
-  @Test
-  @Order(4)
-  public void shouldCreateArtist() {
-    Artist artist = new Artist().firstName("George").lastName("Martin");
-    given().
-      contentType(MediaType.APPLICATION_JSON).
-      body(artist).
-    when().
-      post("/artists").
-    then().
-      assertThat().
-        statusCode(is(201));
-  }
-
-  @Test
-  @Order(5)
-  public void shouldCountArtistAfterCreate() {
-    given().
-    when().
-      get("/artists/count").
-    then().
-      assertThat().
-        statusCode(is(200)).
-      and().
-        body(is("5"));
-  }
-
-
-  @Test
-  @Order(6)
-  public void shouldDeleteArtist() {
-    UUID id = given().get("/artists").then().extract().response().jsonPath().getUUID("id[0]");
-
-    given().
-      pathParam("id", id).
-    when().
-      delete("/artists/{id}").
-    then().
-      assertThat().
-        statusCode(is(204));
-  }
-
-  @Test
-  @Order(7)
-  public void shouldCountArtistAfterDelete() {
-    given().
-    when().
-      get("/artists/count").
-    then().
-      assertThat().
-        statusCode(is(200)).
-      and().
-        body(is("4"));
-  }
-  // end::adocSkip[]
 }
 // end::adocSnippet[]
