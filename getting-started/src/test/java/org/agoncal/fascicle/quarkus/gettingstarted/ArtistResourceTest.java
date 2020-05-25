8ee48af1a9f214e904e10744295a21c16f5d44a7
Antonio Goncalves <antonio.goncalves@gmail.com>
2020-05-25 15:35:19 +0200

    Test

--- getting-started/src/test/java/org/agoncal/fascicle/quarkus/gettingstarted/ArtistResourceTest.java
+++ getting-started/src/test/java/org/agoncal/fascicle/quarkus/gettingstarted/ArtistResourceTest.java
@@ -4,7 +4,7 @@ import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.containsString;
 
 /**
