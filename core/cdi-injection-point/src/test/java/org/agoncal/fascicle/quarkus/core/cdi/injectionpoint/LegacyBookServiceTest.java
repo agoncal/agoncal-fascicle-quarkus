921460ebf54e8c17309028fffb5dcf3e05c5730f
Antonio Goncalves <antonio.goncalves@gmail.com>
2020-06-05 10:22:44 +0200

    injection point

---
+++ core/cdi-injection-point/src/test/java/org/agoncal/fascicle/quarkus/core/cdi/injectionpoint/LegacyBookServiceTest.java
@@ -0,0 +1,30 @@
+package org.agoncal.fascicle.quarkus.core.cdi.injectionpoint;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import javax.inject.Inject;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * @author Antonio Goncalves
+ * http://www.antoniogoncalves.org
+ * --
+ */
+@QuarkusTest
+public class LegacyBookServiceTest {
+
+  @Inject
+  LegacyBookService bookService;
+
+  // ======================================
+  // =              Methods               =
+  // ======================================
+
+  @Test
+  public void shouldCheckNumberIsThirteenDigits() {
+    Book book = bookService.createBook("H2G2", 12.5f, "Geeky scifi Book");
+    assertTrue(book.getIsbn().startsWith("13"));
+  }
+}
