e314d5e31117a098110a1e15aa0b205ea04f7889
Antonio Goncalves <antonio.goncalves@gmail.com>
2020-06-12 16:11:39 +0200

    JPA

---
+++ data/jpa-default/src/main/java/org/agoncal/fascicle/quarkus/data/jpa/dflt/Address.java
@@ -0,0 +1,82 @@
+package org.agoncal.fascicle.quarkus.data.jpa.dflt;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+/**
+ * @author Antonio Goncalves
+ * http://www.antoniogoncalves.org
+ * --
+ */
+// tag::adocSnippet[]
+@Entity
+public class Address {
+
+  @Id
+  @GeneratedValue
+  private Long id;
+  private String street1;
+  private String city;
+  private String zipcode;
+  private String country;
+
+  // Constructors, getters, setters
+  // tag::adocSkip[]
+
+  public Address() {
+  }
+
+  public Address(String street1, String city, String zipcode, String country) {
+    this.street1 = street1;
+    this.city = city;
+    this.zipcode = zipcode;
+    this.country = country;
+  }
+
+  // ======================================
+  // =          Getters & Setters         =
+  // ======================================
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getStreet1() {
+    return street1;
+  }
+
+  public void setStreet1(String street1) {
+    this.street1 = street1;
+  }
+
+  public String getCity() {
+    return city;
+  }
+
+  public void setCity(String city) {
+    this.city = city;
+  }
+
+  public String getZipcode() {
+    return zipcode;
+  }
+
+  public void setZipcode(String zipcode) {
+    this.zipcode = zipcode;
+  }
+
+  public String getCountry() {
+    return country;
+  }
+
+  public void setCountry(String country) {
+    this.country = country;
+  }
+  // end::adocSkip[]
+}
+// end::adocSnippet[]
---
+++ data/jpa-default/src/main/java/org/agoncal/fascicle/quarkus/data/jpa/dflt/Customer.java
@@ -0,0 +1,84 @@
+package org.agoncal.fascicle.quarkus.data.jpa.dflt;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+
+import static javax.persistence.FetchType.LAZY;
+
+
+/**
+ * @author Antonio Goncalves
+ * http://www.antoniogoncalves.org
+ * --
+ */
+// tag::adocSnippet[]
+@Entity
+public class Customer {
+
+  @Id
+  @GeneratedValue
+  private Long id;
+  private String firstName;
+  private String lastName;
+  private String email;
+  @OneToOne(fetch = LAZY)
+  @JoinColumn(name = "address_fk")
+  private Address address;
+
+  // Constructors, getters, setters
+  // tag::adocSkip[]
+
+  public Customer() {
+  }
+
+  public Customer(String firstName, String lastName, String email) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.email = email;
+  }
+
+  // ======================================
+  // =          Getters & Setters         =
+  // ======================================
+
+  public Long getId() {
+    return id;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public Address getAddress() {
+    return address;
+  }
+
+  public void setAddress(Address address) {
+    this.address = address;
+  }
+  // end::adocSkip[]
+}
+// end::adocSnippet[]
