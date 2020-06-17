7fcd734c67f894294b53807b392f17bfd7d65abb
Antonio Goncalves <antonio.goncalves@gmail.com>
2020-06-17 11:51:48 +0200

    Panache Entities

--- data/panache-entities/src/main/java/org/agoncal/fascicle/quarkus/data/panache/model/CD.java
+++ data/panache-entities/src/main/java/org/agoncal/fascicle/quarkus/data/panache/model/CD.java
@@ -14,12 +14,14 @@ import java.util.Set;
  * http://www.antoniogoncalves.org
  * --
  */
+// tag::adocSnippet[]
 @Entity
 public class CD extends Item {
-
+  // tag::adocSkip[]
   // ======================================
   // =             Attributes             =
   // ======================================
+  // end::adocSkip[]
 
   @Column(name = "total_duration")
   public Float totalDuration;
@@ -33,12 +35,16 @@ public class CD extends Item {
   @JoinTable(name = "cd_musician", joinColumns = @JoinColumn(name = "cd_fk"), inverseJoinColumns = @JoinColumn(name = "musician_fk"))
   public Set<Musician> musicians = new HashSet<>();
 
+  // tag::adocSkip[]
   // ======================================
   // =              Methods               =
   // ======================================
-
+  // end::adocSkip[]
+  // tag::adocQuery[]
   public static List<CD> findLikeGenre(String genre) {
     List<CD> cds = list("genre like ?1", "%" + genre + "%");
     return cds;
   }
+  // end::adocQuery[]
 }
+// end::adocSnippet[]
