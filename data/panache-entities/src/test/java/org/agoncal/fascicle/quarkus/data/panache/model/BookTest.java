e90777303c2901c1cf56a6f39b0f61c7d8260cd7
Antonio Goncalves <antonio.goncalves@gmail.com>
2020-06-19 14:34:14 +0200

    Panache

--- data/panache-entities/src/test/java/org/agoncal/fascicle/quarkus/data/panache/model/BookTest.java
+++ data/panache-entities/src/test/java/org/agoncal/fascicle/quarkus/data/panache/model/BookTest.java
@@ -1,5 +1,6 @@
 package org.agoncal.fascicle.quarkus.data.panache.model;
 
+import io.quarkus.hibernate.orm.panache.PanacheQuery;
 import io.quarkus.panache.common.Parameters;
 import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
@@ -8,12 +9,39 @@ import javax.transaction.Transactional;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+// @formatter:off
 @QuarkusTest
 @Transactional
 class BookTest {
+
+  @Test
+  void shouldFind() {
+
+    // tag::adocShouldFind[]
+    // Find returns a PanacheQuery
+    PanacheQuery<Book> bookQuery = Book.find("nbOfPage > 100 ORDER BY title");
+    List<Book> books     = bookQuery.list();
+    Long nbBooks         = bookQuery.count();
+    Book firstBook       = bookQuery.firstResult();
+    Optional<Book> oBook = bookQuery.firstResultOptional();
+    // tag::adocSkip[]
+    assertEquals(5, books.size());
+    assertEquals(5, nbBooks);
+    assertEquals("Beginning Java EE 7", firstBook.title);
+    assertEquals("Beginning Java EE 7", oBook.get().title);
+    // end::adocSkip[]
+
+    // list() is a shortcut to find().list()
+    books = Book.list("nbOfPage > 100 ORDER BY title");
+    // tag::adocSkip[]
+    assertEquals(5, books.size());
+    // end::adocSkip[]
+    // end::adocShouldFind[]
+  }
 
   @Test
   void shouldQuery() {
@@ -50,10 +78,11 @@ class BookTest {
 
     float min = 0f;
     float max = 30f;
+    List<Book> cheapBooks;
 
     // tag::adocShouldQueryWithParameters[]
     // Hard coded parameters
-    List<Book> cheapBooks = Book.list("unitCost between 0 and 30");
+    cheapBooks = Book.list("unitCost between 0 and 30");
     // tag::adocSkip[]
     assertEquals(1, cheapBooks.size());
     // end::adocSkip[]
