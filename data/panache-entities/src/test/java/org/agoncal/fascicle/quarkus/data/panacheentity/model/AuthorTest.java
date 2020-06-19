0859ba92f7d978f0529d3010e41bbacefb71e855
Antonio Goncalves <antonio.goncalves@gmail.com>
2020-06-19 16:35:58 +0200

    Panache Repo

---
+++ data/panache-entities/src/test/java/org/agoncal/fascicle/quarkus/data/panacheentity/model/AuthorTest.java
@@ -0,0 +1,42 @@
+package org.agoncal.fascicle.quarkus.data.panacheentity.model;
+
+import io.quarkus.panache.common.Sort;
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+import static io.quarkus.panache.common.Sort.Direction.Descending;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@QuarkusTest
+@Transactional
+class AuthorTest {
+
+  @Test
+  void shouldSort() {
+
+    List<Author> authors;
+    // tag::adocShouldSort[]
+
+    // Sorts by first name ascending
+    authors = Author.listAll(Sort.by("firstName"));
+    // tag::adocSkip[]
+    assertEquals(14, authors.size());
+    // end::adocSkip[]
+
+    // Sorts by first name descending
+    authors = Author.listAll(Sort.by("firstName", Descending));
+    // tag::adocSkip[]
+    assertEquals(14, authors.size());
+    // end::adocSkip[]
+
+    // Sorts by first name ascending and last name descending
+    authors = Author.listAll(Sort.by("firstName").and("lastName", Descending));
+    // tag::adocSkip[]
+    assertEquals(14, authors.size());
+    // end::adocSkip[]
+    // end::adocShouldFind[]
+  }
+}
