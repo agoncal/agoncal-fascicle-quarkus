9bfc248e0b16bd059b6185f507af228289deb678
Antonio Goncalves <antonio.goncalves@gmail.com>
2020-06-19 14:02:46 +0200

    Panache

--- data/panache-entities/src/test/java/org/agoncal/fascicle/quarkus/data/panache/model/BookTest.java
+++ data/panache-entities/src/test/java/org/agoncal/fascicle/quarkus/data/panache/model/BookTest.java
@@ -5,7 +5,9 @@ import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
 
 import javax.transaction.Transactional;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -46,44 +48,43 @@ class BookTest {
   @Test
   void shouldQueryWithParameters() {
 
-    List<Book> books;
+    float min = 0f;
+    float max = 30f;
+
     // tag::adocShouldQueryWithParameters[]
-    // Full JPQL query
-    books = Book.list("SELECT b FROM Book b WHERE b.nbOfPage > 100 ORDER BY b.title");
+    // Hard coded parameters
+    List<Book> cheapBooks = Book.list("unitCost between 0 and 30");
     // tag::adocSkip[]
-    assertEquals(5, books.size());
+    assertEquals(1, cheapBooks.size());
     // end::adocSkip[]
 
-    // Simplified JPQL query
-    books = Book.list("FROM Book b WHERE b.nbOfPage > 100 ORDER BY b.title");
+    // Position parameters
+    cheapBooks = Book.list("unitCost between ?1 and ?2", min, max);
     // tag::adocSkip[]
-    assertEquals(5, books.size());
+    assertEquals(1, cheapBooks.size());
     // end::adocSkip[]
 
-    // Very simplified JPQL query
-    books = Book.list("nbOfPage > 100 ORDER BY title");
+    // Named parameters
+    Map<String, Object> params = new HashMap<>();
+    params.put("min", min);
+    params.put("max", max);
+    cheapBooks = Book.list("unitCost between :min and :max", params);
     // tag::adocSkip[]
-    assertEquals(5, books.size());
+    assertEquals(1, cheapBooks.size());
     // end::adocSkip[]
 
-    List<Book> englishBooks = Book.list("language", Language.ENGLISH);
-    // tag::adocSkip[]
-    assertEquals(4, englishBooks.size());
-    // end::adocSkip[]
-
-    long nbEnglishBooks = Book.count("language", Language.ENGLISH);
-    // tag::adocSkip[]
-    assertEquals(4, nbEnglishBooks);
-
-    float min = 0;
-    float max = 30;
-    List<Book> cheapBooks;
-    // end::adocSkip[]
-
+    // Using the Parameters class
     cheapBooks = Book.list("unitCost between :min and :max",
       Parameters.with("min", min).and("max", max));
     // tag::adocSkip[]
     assertEquals(1, cheapBooks.size());
+    // end::adocShouldQueryWithParameters[]
+
+    // Passing an enumeration
+    List<Book> englishBooks = Book.list("language", Language.ENGLISH);
+    // tag::adocSkip[]
+    assertEquals(4, englishBooks.size());
+    // end::adocSkip[]
     // end::adocShouldQueryWithParameters[]
   }
 }
