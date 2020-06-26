b7b4d86bd7313e1b8d98858faa91d1fed2513781
Antonio Goncalves <antonio.goncalves@gmail.com>
2020-06-26 16:10:25 +0200

    OpenAPI Custom

--- http/open-api-custom/src/main/java/org/agoncal/fascicle/quarkus/http/openapi/custom/AuthorResource.java
+++ http/open-api-custom/src/main/java/org/agoncal/fascicle/quarkus/http/openapi/custom/AuthorResource.java
@@ -1,10 +1,7 @@
-package org.agoncal.fascicle.quarkus.http.openapi;
+package org.agoncal.fascicle.quarkus.http.openapi.custom;
 
 import org.eclipse.microprofile.openapi.annotations.Operation;
-import org.eclipse.microprofile.openapi.annotations.media.Content;
-import org.eclipse.microprofile.openapi.annotations.parameters.Parameter;
 import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
-import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -17,9 +14,9 @@ import javax.ws.rs.core.MediaType;
  * http://www.antoniogoncalves.org
  * --
  */
+// tag::adocSnippet[]
 @Path("/authors")
 @Produces(MediaType.TEXT_PLAIN)
-@Tag(name = "Author Endpoint")
 public class AuthorResource {
 
   String[] scifiAuthors = {"Isaac Asimov", "Ray Bradbury", "Douglas Adams"};
@@ -27,9 +24,10 @@ public class AuthorResource {
   @GET
   @Path("/{index}")
   @Operation(summary = "Returns an author for a given index")
-  @APIResponse(responseCode = "200", content = @Content(mediaType = MediaType.TEXT_PLAIN))
-  @APIResponse(responseCode = "204", description = "The author is not found for a given index")
-  public String getScifiAuthor(@Parameter(description = "Author index", required = true) @PathParam("index") int index) {
+  @APIResponse(responseCode = "200", description = "Author returned for a given index")
+  @APIResponse(responseCode = "204", description = "Author not found")
+  public String getScifiAuthor(@PathParam("index") int index) {
     return scifiAuthors[index];
   }
 }
+// end::adocSnippet[]
---
+++ http/open-api-custom/src/test/java/org/agoncal/fascicle/quarkus/http/openapi/custom/AuthorResourceTest.java
@@ -0,0 +1,50 @@
+package org.agoncal.fascicle.quarkus.http.openapi.custom;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static javax.ws.rs.core.HttpHeaders.ACCEPT;
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+import static javax.ws.rs.core.Response.Status.OK;
+import static org.hamcrest.CoreMatchers.is;
+
+/**
+ * @author Antonio Goncalves
+ * http://www.antoniogoncalves.org
+ * --
+ */
+//@formatter:off
+@QuarkusTest
+public class AuthorResourceTest {
+
+  @Test
+  public void shouldGetAnAuthor() {
+    given()
+      .pathParam("index", 0).
+    when()
+      .get("/authors/{index}").
+    then()
+      .statusCode(OK.getStatusCode())
+      .body(is("Isaac Asimov"));
+  }
+
+  @Test
+  void shouldPingOpenAPI() {
+    given()
+      .header(ACCEPT, APPLICATION_JSON).
+      when()
+      .get("/openapi").
+      then()
+      .statusCode(OK.getStatusCode());
+  }
+
+  @Test
+  void shouldPingSwaggerUI() {
+    given().
+      when()
+      .get("/swagger-ui").
+      then()
+      .statusCode(OK.getStatusCode());
+  }
+}
