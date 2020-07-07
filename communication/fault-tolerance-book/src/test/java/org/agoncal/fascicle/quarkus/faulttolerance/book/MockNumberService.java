0f86c8eb13458edbe744eb094ca46682211cff21
Antonio Goncalves <antonio.goncalves@gmail.com>
2020-07-07 17:20:02 +0200

    Fault Tolerance

--- communication/fault-tolerance-book/src/test/java/org/agoncal/fascicle/quarkus/faulttolerance/book/MockNumberService.java
+++ communication/fault-tolerance-book/src/test/java/org/agoncal/fascicle/quarkus/faulttolerance/book/MockNumberService.java
@@ -5,12 +5,21 @@ import io.quarkus.test.Mock;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.json.Json;
+import javax.json.JsonObject;
 
 // tag::adocSnippet[]
 @Mock
 @ApplicationScoped
 @RestClient
 public class MockNumberService implements NumberService {
+
+  @Override
+  public JsonObject generateIssn() {
+    return Json.createObjectBuilder()
+      .add("isbn10", "dummy isbn 10")
+      .build();
+  }
 
   @Override
   public IsbnNumber generateIsbn(boolean separator) {
