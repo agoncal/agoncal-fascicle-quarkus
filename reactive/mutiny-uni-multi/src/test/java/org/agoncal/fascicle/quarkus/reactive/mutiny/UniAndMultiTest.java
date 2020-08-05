97afe2d3ce2ee0eaba1fbef8f300f3451c8278a6
Antonio Goncalves <antonio.goncalves@gmail.com>
2020-08-05 14:44:34 +0200

    Mutiny

---
+++ reactive/mutiny-uni-multi/src/test/java/org/agoncal/fascicle/quarkus/reactive/mutiny/UniAndMultiTest.java
@@ -0,0 +1,21 @@
+package org.agoncal.fascicle.quarkus.reactive.mutiny;
+
+import io.smallrye.mutiny.operators.UniNever;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class UniAndMultiTest {
+
+  UniAndMulti uniAndMulti = new UniAndMulti();
+
+  @Test
+  void shouldCreateUniFromNothing() {
+    assertEquals(UniNever.INSTANCE, uniAndMulti.createUniFromNothing());
+  }
+
+  @Test
+  void shouldCreateUniFromFailure() {
+    assertEquals("Hello world", uniAndMulti.createUniFromFailure());
+  }
+}
