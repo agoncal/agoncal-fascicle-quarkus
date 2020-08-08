3ad5390f005948ae2a8a7d1c375e746eeeafddb1
Antonio Goncalves <antonio.goncalves@gmail.com>
2020-08-08 21:52:52 +0200

    Cloud

--- first-step/src/test/java/org/agoncal/fascicle/quarkus/firststep/AuthorResourceTest.java
+++ first-step/src/test/java/org/agoncal/fascicle/quarkus/firststep/AuthorResourceTest.java
@@ -3,7 +3,9 @@ package org.agoncal.fascicle.quarkus.firststep;
 import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
 
-import static io.restassured.RestAssured.get;
+import static io.restassured.RestAssured.given;
+import static javax.ws.rs.core.HttpHeaders.ACCEPT;
+import static javax.ws.rs.core.MediaType.TEXT_PLAIN;
 import static org.hamcrest.core.Is.is;
 
 /**
@@ -18,7 +20,10 @@ public class AuthorResourceTest {
 
   @Test
   public void shouldGetAllAuthors() {
-    get("/authors").
+    given()
+      .header(ACCEPT, TEXT_PLAIN).
+    when()
+      .get("/authors").
     then()
       .assertThat()
         .statusCode(is(200))
@@ -28,7 +33,11 @@ public class AuthorResourceTest {
 
   @Test
   public void shouldGetAnAuthor() {
-    get("/authors/0").
+    given()
+      .header(ACCEPT, TEXT_PLAIN)
+      .pathParam("index", 0).
+    when()
+      .get("/authors/{index}").
     then()
       .assertThat()
         .statusCode(is(200))
