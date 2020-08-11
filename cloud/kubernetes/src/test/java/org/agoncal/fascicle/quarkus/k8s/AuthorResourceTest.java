6704cd1e870d69d6cfde596f64391c095fc4f96b
Antonio Goncalves <antonio.goncalves@gmail.com>
2020-08-11 16:05:26 +0200

    Docker

---
+++ cloud/kubernetes/src/test/java/org/agoncal/fascicle/quarkus/k8s/AuthorResourceTest.java
@@ -0,0 +1,39 @@
+package org.agoncal.fascicle.quarkus.k8s;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.get;
+import static org.hamcrest.core.Is.is;
+
+/**
+ * @author Antonio Goncalves
+ * http://www.antoniogoncalves.org
+ * --
+ */
+// @formatter:off
+// tag::adocSnippet[]
+@QuarkusTest
+public class AuthorResourceTest {
+
+  @Test
+  public void shouldGetAllAuthors() {
+    get("/authors").
+    then()
+      .assertThat()
+        .statusCode(is(200))
+      .and()
+        .body(is("Isaac Asimov, Ray Bradbury, Douglas Adams"));
+  }
+
+  @Test
+  public void shouldGetAnAuthor() {
+    get("/authors/0").
+    then()
+      .assertThat()
+        .statusCode(is(200))
+      .and()
+        .body(is("Isaac Asimov"));
+  }
+}
+// end::adocSnippet[]
