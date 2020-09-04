eb7cc8423206a312173c5c3bf09582c451dbd3fe
Antonio Goncalves <antonio.goncalves@gmail.com>
2020-09-04 11:13:06 +0200

    test

--- testing/test-transactional/src/test/java/org/agoncal/fascicle/quarkus/test/transactional/MusicianTest.java
+++ testing/test-transactional/src/test/java/org/agoncal/fascicle/quarkus/test/transactional/MusicianTest.java
@@ -1,59 +1,29 @@
 package org.agoncal.fascicle.quarkus.test.transactional;
 
-import io.quarkus.hibernate.orm.panache.PanacheQuery;
-import io.quarkus.panache.common.Page;
 import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
 
 import javax.transaction.Transactional;
-import java.util.List;
+import java.time.LocalDate;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+// tag::adocSnippet[]
 @QuarkusTest
 @Transactional
 class MusicianTest {
 
   @Test
-  void shouldPaginate() {
+  void shouldPersistAMusician() {
+    Musician musician = new Musician();
+    musician.firstName = "Janis";
+    musician.lastName = "Joplin";
+    musician.dateOfBirth = LocalDate.of(1943, 01, 19);
+    musician.preferredInstrument = "Voice";
 
-    // tag::adocSnippet[]
-    // Create a query for all musicians
-    PanacheQuery<Musician> musicianQuery = Musician.findAll();
-    // tag::adocSkip[]
-    assertEquals(13, musicianQuery.count());
-    // end::adocSkip[]
+    Musician.persist(musician);
 
-    // Make it use pages of 5 entries at a time
-    musicianQuery.page(Page.ofSize(5));
-
-    // Get the first page
-    List<Musician> firstPage = musicianQuery.list();
-    // tag::adocSkip[]
-    assertEquals(5, firstPage.size());
-    // end::adocSkip[]
-
-    // Get the second page
-    List<Musician> secondPage = musicianQuery.nextPage().list();
-    // tag::adocSkip[]
-    assertEquals(5, secondPage.size());
-    // end::adocSkip[]
-
-    // Get the third page
-    List<Musician> lastPage = musicianQuery.nextPage().list();
-    // tag::adocSkip[]
-    assertEquals(3, lastPage.size());
-    // end::adocSkip[]
-
-    // Get page 3 using index
-    List<Musician> page3 = musicianQuery.page(Page.of(2, 5)).list();
-    // tag::adocSkip[]
-    assertEquals(3, page3.size());
-    // end::adocSkip[]
-
-    // Get the number of pages
-    int numberOfPages = musicianQuery.pageCount();
-    // end::adocSnippet[]
-    assertEquals(3, numberOfPages);
+    assertNotNull(musician.id);
   }
 }
+// end::adocSnippet[]
