da5433bc4b30df6423fcf1c9044b4d3d2bed07e2
Antonio Goncalves <antonio.goncalves@gmail.com>
2020-09-07 15:10:15 +0200

    mock

---
+++ testing/mock-alternative/src/main/java/org/agoncal/fascicle/quarkus/test/mock/alternative/IsbnServiceProxy.java
@@ -0,0 +1,19 @@
+package org.agoncal.fascicle.quarkus.test.mock.alternative;
+
+import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+// tag::adocSnippet[]
+@Path("/api/numbers")
+@Produces(MediaType.APPLICATION_JSON)
+@RegisterRestClient
+public interface IsbnServiceProxy {
+
+  @GET
+  IsbnNumbers generateIsbnNumbers();
+}
+// end::adocSnippet[]
---
+++ testing/mock-alternative/src/test/java/org/agoncal/fascicle/quarkus/test/mock/alternative/MockIsbnServiceProxy.java
@@ -0,0 +1,25 @@
+// tag::adocTest[]
+package org.agoncal.fascicle.quarkus.test.mock.alternative;
+
+import org.eclipse.microprofile.rest.client.inject.RestClient;
+
+import javax.annotation.Priority;
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.inject.Alternative;
+
+// tag::adocSnippet[]
+@Alternative
+@Priority(1)
+@ApplicationScoped
+@RestClient
+public class MockIsbnServiceProxy implements IsbnServiceProxy {
+
+  @Override
+  public IsbnNumbers generateIsbnNumbers() {
+    IsbnNumbers isbnNumbers = new IsbnNumbers();
+    isbnNumbers.setIsbn13("@Alternative isbn 13");
+    isbnNumbers.setIsbn10("@Alternative isbn 10");
+    return isbnNumbers;
+  }
+}
+// end::adocSnippet[]
