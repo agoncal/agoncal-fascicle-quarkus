cfa4133fa7fe4d8237f551c6fcf51c03e9a7fb47
Antonio Goncalves <antonio.goncalves@gmail.com>
2020-09-22 09:33:34 +0200

    Messages (adoc)

--- reactive/messages-kafka/src/main/java/org/agoncal/fascicle/quarkus/reactive/messages/kafka/codecs/PurchaseOrderDeserializer.java
+++ reactive/messages-kafka/src/main/java/org/agoncal/fascicle/quarkus/reactive/messages/kafka/codecs/PurchaseOrderDeserializer.java
@@ -3,8 +3,10 @@ package org.agoncal.fascicle.quarkus.reactive.messages.kafka.codecs;
 import org.agoncal.fascicle.quarkus.reactive.messages.kafka.model.PurchaseOrder;
 import io.quarkus.kafka.client.serialization.JsonbDeserializer;
 
+// tag::adocSnippet[]
 public class PurchaseOrderDeserializer extends JsonbDeserializer<PurchaseOrder> {
   public PurchaseOrderDeserializer() {
     super(PurchaseOrder.class);
   }
 }
+// end::adocSnippet[]
