5c62f18822c7ba8744bcfa402e9da9b1c68836ea
Antonio Goncalves <antonio.goncalves@gmail.com>
2020-09-25 13:32:28 +0200

    Mutiny

--- reactive/mutiny-uni-multi/src/main/java/org/agoncal/fascicle/quarkus/reactive/mutiny/CreateUniLazily.java
+++ reactive/mutiny-uni-multi/src/main/java/org/agoncal/fascicle/quarkus/reactive/mutiny/CreateUniLazily.java
@@ -5,6 +5,7 @@ import io.smallrye.mutiny.Uni;
 public class CreateUniLazily {
 
   public static void main(String[] args) {
+    // tag::adocSnippet[]
     // Lazily creates a Uni
     Uni<String> uni = Uni.createFrom().item("Terri Lyne Carrington")
       .onItem().transform(s -> s.toUpperCase() + " ");
@@ -12,4 +13,5 @@ public class CreateUniLazily {
     // Subscribes to it
     uni.subscribe().with(System.out::print);
   }
+  // end::adocSnippet[]
 }
