1d00898bb9f4db0b2b66febc27d08d5b5940fe48
Antonio Goncalves <antonio.goncalves@gmail.com>
2020-09-25 13:10:26 +0200

    Mutiny

--- reactive/mutiny-uni-multi/src/main/java/org/agoncal/fascicle/quarkus/reactive/mutiny/CreateMultiWithAllEvents.java
+++ reactive/mutiny-uni-multi/src/main/java/org/agoncal/fascicle/quarkus/reactive/mutiny/CreateMultiWithAllEvents.java
@@ -7,11 +7,11 @@ public class CreateMultiWithAllEvents {
   public static void main(String[] args) {
 
     // tag::adocSnippet[]
-    Multi.createFrom().items("a", "b", "c")
-      .onItem().invoke(item -> System.out.println("Received item " + item))
+    Multi.createFrom().items("Carla Bley", "John Coltrane", "Miles Davis", "Juliette Gréco")
+      .onItem().invoke(item -> System.out.println("Received   " + item))
       .onCompletion().invoke(() -> System.out.println("Completed"))
       .onFailure().invoke(failure -> System.out.println("Completed with failure " + failure.getMessage()))
-      .subscribe().with(item -> System.out.println("Subscriber received " + item));
+      .subscribe().with(item -> System.out.println("Subscriber " + item));
     // end::adocSnippet[]
   }
 }
--- reactive/mutiny-uni-multi/src/main/java/org/agoncal/fascicle/quarkus/reactive/mutiny/CreateUniWithAllEvents.java
+++ reactive/mutiny-uni-multi/src/main/java/org/agoncal/fascicle/quarkus/reactive/mutiny/CreateUniWithAllEvents.java
@@ -7,10 +7,10 @@ public class CreateUniWithAllEvents {
   public static void main(String[] args) {
 
     // tag::adocSnippet[]
-    Uni.createFrom().item("a")
-      .onItem().invoke(item -> System.out.println("Received item " + item))
+    Uni.createFrom().item("Carla Bley")
+      .onItem().invoke(item -> System.out.println("Received   " + item))
       .onFailure().invoke(failure -> System.out.println("Completed with failure " + failure.getMessage()))
-      .subscribe().with(item -> System.out.println("Subscriber received " + item));
+      .subscribe().with(item -> System.out.println("Subscriber " + item));
     // end::adocSnippet[]
   }
 }
