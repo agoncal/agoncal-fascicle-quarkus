139da9bc19a2ed438be87e21c65df54fd2555d51
Antonio Goncalves <antonio.goncalves@gmail.com>
2020-09-29 17:00:18 +0200

    Putting together

---
+++ putting-together/rest-number/src/test/java/org/agoncal/fascicle/quarkus/number/NativeNumberResourceIT.java
@@ -0,0 +1,9 @@
+package org.agoncal.fascicle.quarkus.number;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeNumberResourceIT extends NumberResourceTest {
+
+    // Execute the same tests but in native mode.
+}
