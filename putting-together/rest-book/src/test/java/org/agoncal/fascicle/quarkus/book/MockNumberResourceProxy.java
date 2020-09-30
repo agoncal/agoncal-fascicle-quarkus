d56006c9f7eb5a25d9991313a9ee98356fe49783
Antonio Goncalves <antonio.goncalves@gmail.com>
2020-09-30 11:55:21 +0200

    Putting together

--- putting-together/rest-book/src/test/java/org/agoncal/fascicle/quarkus/book/MockNumberResourceProxy.java
+++ putting-together/rest-book/src/test/java/org/agoncal/fascicle/quarkus/book/MockNumberResourceProxy.java
@@ -10,7 +10,7 @@ import javax.enterprise.context.ApplicationScoped;
 @Mock
 @ApplicationScoped
 @RestClient
-public class MockIsbnNumbersService implements NumberResourceProxy {
+public class MockNumberResourceProxy implements NumberResourceProxy {
 
   @Override
   public IsbnNumbers generateIsbnNumbers() {
