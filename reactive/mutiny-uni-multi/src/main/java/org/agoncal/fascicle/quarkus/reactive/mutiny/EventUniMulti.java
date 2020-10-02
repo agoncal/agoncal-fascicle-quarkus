481931346f412006e440e6ce8ec5fb496bd52a04
Antonio Goncalves <antonio.goncalves@gmail.com>
2020-10-02 13:04:50 +0200

    Uni

--- reactive/mutiny-uni-multi/src/main/java/org/agoncal/fascicle/quarkus/reactive/mutiny/EventUniMulti.java
+++ reactive/mutiny-uni-multi/src/main/java/org/agoncal/fascicle/quarkus/reactive/mutiny/EventUniMulti.java
@@ -16,17 +16,17 @@ public class EventUniMulti {
     // tag::adocUniEvent[]
     Uni.createFrom().item("Carla Bley")
       .onItem().invoke(item -> System.out.println("Received   " + item))
-      .onFailure().invoke(failure -> System.out.println("Completed with failure " + failure.getMessage()))
+      .onFailure().invoke(failure -> System.out.println("Failed " + failure.getMessage()))
       .subscribe().with(item -> System.out.println("Subscriber " + item));
     // end::adocUniEvent[]
   }
 
   private static void multiEvent() {
     // tag::adocMultiEvent[]
-    Multi.createFrom().items("Carla Bley", "John Coltrane", "Miles Davis", "Juliette Gréco")
+    Multi.createFrom().items("Carla Bley", "John Coltrane", "Juliette Gréco")
       .onItem().invoke(item -> System.out.println("Received   " + item))
       .onCompletion().invoke(() -> System.out.println("Completed"))
-      .onFailure().invoke(failure -> System.out.println("Completed with failure " + failure.getMessage()))
+      .onFailure().invoke(failure -> System.out.println("Failed " + failure.getMessage()))
       .subscribe().with(item -> System.out.println("Subscriber " + item));
     // end::adocMultiEvent[]
   }
