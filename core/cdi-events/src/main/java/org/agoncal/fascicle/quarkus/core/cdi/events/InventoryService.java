7b08a5ee11a838bc531b8d3429d75cb9d4de5e14
Antonio Goncalves <antonio.goncalves@gmail.com>
2020-10-09 15:21:04 +0200

    Log

--- core/cdi-events/src/main/java/org/agoncal/fascicle/quarkus/core/cdi/events/InventoryService.java
+++ core/cdi-events/src/main/java/org/agoncal/fascicle/quarkus/core/cdi/events/InventoryService.java
@@ -1,9 +1,11 @@
 package org.agoncal.fascicle.quarkus.core.cdi.events;
 
 import javax.enterprise.event.Observes;
+import javax.inject.Inject;
 import javax.inject.Singleton;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Logger;
 
 /**
  * @author Antonio Goncalves
@@ -14,9 +16,17 @@ import java.util.List;
 @Singleton
 public class InventoryService {
 
+  // tag::adocSkip[]
+  @Inject
+  Logger logger;
+
+  // end::adocSkip[]
   List<Book> inventory = new ArrayList<>();
 
   public void addBook(@Observes Book book) {
+    // tag::adocSkip[]
+    logger.info("Adding book " + book.getTitle() + " to inventory");
+    // end::adocSkip[]
     inventory.add(book);
   }
 }
