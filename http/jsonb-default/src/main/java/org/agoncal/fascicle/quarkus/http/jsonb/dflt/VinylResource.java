8c364052d83c03b813eb211b8437d8297546b8d9
Antonio Goncalves <antonio.goncalves@gmail.com>
2020-10-16 14:30:58 +0200

    JSonb injection

---
+++ http/jsonb-default/src/main/java/org/agoncal/fascicle/quarkus/http/jsonb/dflt/VinylResource.java
@@ -0,0 +1,26 @@
+package org.agoncal.fascicle.quarkus.http.jsonb.dflt;
+
+import javax.inject.Inject;
+import javax.json.bind.Jsonb;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+
+// tag::adocSnippet[]
+@Path("/api/vinyl")
+public class VinylResource {
+
+  @Inject
+  Jsonb jsonb;
+
+  @GET
+  @Produces(APPLICATION_JSON)
+  public String getVinyl() {
+
+    Vinyl vinyl = new Vinyl().title("Horses").artist("Patti Smith").musicCompany("Arista Records");
+    return jsonb.toJson(vinyl);
+  }
+}
+// end::adocSnippet[]
