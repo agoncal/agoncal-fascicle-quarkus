41d337570d5f7fb0a0b75ede62efcd590e2ae4ee
Antonio Goncalves <antonio.goncalves@gmail.com>
2020-10-21 16:57:45 +0200

    Bump Quarkus

--- putting-together/rest-book/bootstrap.sh
+++ putting-together/rest-book/bootstrap.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 # tag::adocSnippet[]
-mvn io.quarkus:quarkus-maven-plugin:1.8.1.Final:create \
-     -DprojectGroupId=org.agoncal.fascicle.quarkus.putting-together \
-     -DprojectArtifactId=rest-book \
-     -DprojectVersion=1.0-SNAPSHOT \
-     -DclassName="org.agoncal.fascicle.quarkus.book.BookResource" \
-     -Dpath="/api/books" \
-     -Dextensions="resteasy-jsonb, rest-client, smallrye-fault-tolerance, smallrye-metrics"
+mvn io.quarkus:quarkus-maven-plugin:1.9.0.Final:create \
+    -DplatformVersion=1.9.0.Final \
+    -DprojectGroupId=org.agoncal.fascicle.quarkus.putting-together \
+    -DprojectArtifactId=rest-book \
+    -DprojectVersion=1.0-SNAPSHOT \
+    -DclassName="org.agoncal.fascicle.quarkus.book.BookResource" \
+    -Dpath="/api/books" \
+    -Dextensions="resteasy-jsonb, rest-client, smallrye-fault-tolerance, smallrye-metrics"
 # end::adocSnippet[]
