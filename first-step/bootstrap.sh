d54d5770ee5a89ecc766ab91e33c2b0b3071a36f
Antonio Goncalves <antonio.goncalves@gmail.com>
2021-05-03 21:27:02 +0200

    Bump Quarkus versions

--- first-step/bootstrap.sh
+++ first-step/bootstrap.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-mvn io.quarkus:quarkus-maven-plugin:1.8.1.Final:create \
+mvn io.quarkus:quarkus-maven-plugin:1.13.2.Final:create \
     -DprojectGroupId=org.agoncal.fascicle.quarkus \
     -DprojectArtifactId=first-step \
     -DclassName="org.agoncal.fascicle.quarkus.firststep.AuthorResource" \
--- putting-together/rest-book/bootstrap.sh
+++ putting-together/rest-book/bootstrap.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # tag::adocSnippet[]
-mvn io.quarkus:quarkus-maven-plugin:1.9.0.Final:create \
-    -DplatformVersion=1.9.0.Final \
+mvn io.quarkus:quarkus-maven-plugin:1.13.2.Final:create \
+    -DplatformVersion=1.13.2.Final \
     -DprojectGroupId=org.agoncal.fascicle.quarkus.putting-together \
     -DprojectArtifactId=rest-book \
     -DprojectVersion=1.0-SNAPSHOT \
