18bf4b7c4168aa10f6a66a51ab48a003f0b13217
Antonio Goncalves <antonio.goncalves@gmail.com>
2021-11-01 17:24:54 +0100

    Documentation

--- putting-together/rest-number/bootstrap.sh
+++ putting-together/rest-number/bootstrap.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
 # tag::adocSnippet[]
-mvn io.quarkus:quarkus-maven-plugin:1.13.2.Final:create \
-    -DplatformVersion=1.13.2.Final \
+mvn io.quarkus:quarkus-maven-plugin:2.4.0.Final:create \
     -DprojectGroupId=org.agoncal.fascicle.quarkus.putting-together \
     -DprojectArtifactId=rest-number \
-    -DprojectVersion=1.0-SNAPSHOT \
     -DclassName="org.agoncal.fascicle.quarkus.number.NumberResource" \
     -Dpath="/api/numbers" \
     -Dextensions="resteasy-jsonb, smallrye-openapi, smallrye-health"
