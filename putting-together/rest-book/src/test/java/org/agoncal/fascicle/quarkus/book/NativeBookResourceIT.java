7283343dd2fdca0adf3d2c0c7fd9bff93d09115d
Antonio Goncalves <antonio.goncalves@gmail.com>
2022-10-22 15:08:54 +0200

    Putting Together

--- putting-together/rest-book/src/test/java/org/agoncal/fascicle/quarkus/book/NativeBookResourceIT.java
+++
@@ -1,9 +0,0 @@
-package org.agoncal.fascicle.quarkus.book;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeBookResourceIT extends BookResourceTest {
-
-    // Execute the same tests but in native mode.
-}
---
+++ putting-together/rest-number/src/test/java/org/agoncal/fascicle/quarkus/number/NumberResourceIT.java
@@ -0,0 +1,8 @@
+package org.agoncal.fascicle.quarkus.number;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class NumberResourceIT extends NumberResourceTest {
+    // Execute the same tests but in packaged mode.
+}
