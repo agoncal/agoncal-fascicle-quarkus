92407e9f93711dbe37baccc487ed701d4b53dcdc
Antonio Goncalves <antonio.goncalves@gmail.com>
2022-10-22 14:57:38 +0200

    Getting Started

---
+++ getting-started/src/test/java/org/agoncal/fascicle/quarkus/gettingstarted/ArtistResourceIT.java
@@ -0,0 +1,15 @@
+package org.agoncal.fascicle.quarkus.gettingstarted;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+/**
+ * @author Antonio Goncalves
+ * http://www.antoniogoncalves.org
+ * --
+ */
+// tag::adocSnippet[]
+@QuarkusIntegrationTest
+public class ArtistResourceIT extends ArtistResourceTest {
+    // Execute the same tests but in packaged mode.
+}
+// end::adocSnippet[]
