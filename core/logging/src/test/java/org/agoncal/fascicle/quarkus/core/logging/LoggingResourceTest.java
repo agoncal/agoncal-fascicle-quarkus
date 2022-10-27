ad6b6ccfaa1ce1ebad5d44f56c690231b44be02d
Antonio Goncalves <antonio.goncalves@gmail.com>
2022-10-27 04:59:15 +0200

    3 Alpha Logging

--- core/logging/src/test/java/org/agoncal/fascicle/quarkus/core/logging/LoggingResourceTest.java
+++ core/logging/src/test/java/org/agoncal/fascicle/quarkus/core/logging/LoggingResourceTest.java
@@ -4,7 +4,7 @@ import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
-import static javax.ws.rs.core.Response.Status.NO_CONTENT;
+import static jakarta.ws.rs.core.Response.Status.NO_CONTENT;
 
 /**
  * @author Antonio Goncalves
