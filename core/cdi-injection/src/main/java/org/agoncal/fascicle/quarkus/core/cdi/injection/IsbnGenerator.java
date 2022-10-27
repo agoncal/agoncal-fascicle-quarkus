dec35bbc91eb2a1d5e33ca63c4f985cae9bc0348
Antonio Goncalves <antonio.goncalves@gmail.com>
2022-10-27 04:46:31 +0200

    3 Alpha CDI Injection

--- core/cdi-injection/src/main/java/org/agoncal/fascicle/quarkus/core/cdi/injection/IsbnGenerator.java
+++ core/cdi-injection/src/main/java/org/agoncal/fascicle/quarkus/core/cdi/injection/IsbnGenerator.java
@@ -1,7 +1,7 @@
 package org.agoncal.fascicle.quarkus.core.cdi.injection;
 
 
-import javax.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.context.ApplicationScoped;
 import java.util.Random;
 
 /**
