09af4d095d803a6d50ed83e8fb5637f5484a682a
Antonio Goncalves <antonio.goncalves@gmail.com>
2022-10-27 04:45:39 +0200

    3 Alpha CDI Events Qualifier

--- core/cdi-events-qualifiers/src/main/java/org/agoncal/fascicle/quarkus/core/cdi/events/BookService.java
+++ core/cdi-events-qualifiers/src/main/java/org/agoncal/fascicle/quarkus/core/cdi/events/BookService.java
@@ -1,8 +1,8 @@
 package org.agoncal.fascicle.quarkus.core.cdi.events;
 
-import javax.enterprise.context.ApplicationScoped;
-import javax.enterprise.event.Event;
-import javax.inject.Inject;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.event.Event;
+import jakarta.inject.Inject;
 
 /**
  * @author Antonio Goncalves
