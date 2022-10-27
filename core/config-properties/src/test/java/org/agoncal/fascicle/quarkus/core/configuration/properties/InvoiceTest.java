4009beb8c07c6459b009668f93c175f0c7fce4e9
Antonio Goncalves <antonio.goncalves@gmail.com>
2022-10-27 04:51:52 +0200

    3 Alpha Config Prop

--- core/config-properties/src/test/java/org/agoncal/fascicle/quarkus/core/configuration/properties/InvoiceTest.java
+++ core/config-properties/src/test/java/org/agoncal/fascicle/quarkus/core/configuration/properties/InvoiceTest.java
@@ -3,7 +3,7 @@ package org.agoncal.fascicle.quarkus.core.configuration.properties;
 import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
 
-import javax.inject.Inject;
+import jakarta.inject.Inject;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
