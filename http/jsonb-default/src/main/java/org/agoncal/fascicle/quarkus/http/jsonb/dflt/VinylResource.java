d5d46d66d3dd26c73b7383ee82267ebe6191019f
Antonio Goncalves <antonio.goncalves@gmail.com>
2022-10-28 00:55:27 +0200

    3 Alpha JSONB Default

--- http/jsonb-default/src/main/java/org/agoncal/fascicle/quarkus/http/jsonb/dflt/VinylResource.java
+++ http/jsonb-default/src/main/java/org/agoncal/fascicle/quarkus/http/jsonb/dflt/VinylResource.java
@@ -1,12 +1,12 @@
 package org.agoncal.fascicle.quarkus.http.jsonb.dflt;
 
-import javax.inject.Inject;
-import javax.json.bind.Jsonb;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import jakarta.inject.Inject;
+import jakarta.json.bind.Jsonb;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
 
-import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+import static jakarta.ws.rs.core.MediaType.APPLICATION_JSON;
 
 // @formatter:off
 // tag::adocSnippet[]
--- http/jsonb-default/src/test/java/org/agoncal/fascicle/quarkus/http/jsonb/dflt/BookResourceTest.java
+++ http/jsonb-default/src/test/java/org/agoncal/fascicle/quarkus/http/jsonb/dflt/BookResourceTest.java
@@ -5,9 +5,9 @@ import org.hamcrest.core.Is;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
-import static javax.ws.rs.core.HttpHeaders.ACCEPT;
-import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
-import static javax.ws.rs.core.Response.Status.OK;
+import static jakarta.ws.rs.core.HttpHeaders.ACCEPT;
+import static jakarta.ws.rs.core.MediaType.APPLICATION_JSON;
+import static jakarta.ws.rs.core.Response.Status.OK;
 
 // @formatter:off
 @QuarkusTest
