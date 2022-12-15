a71459c6646af65e7f0fda38cdd59c3263a79cd4
Antonio Goncalves <antonio.goncalves@gmail.com>
2022-12-15 15:48:44 +0100

    Alpha2

--- first-look/bootstrap.sh
+++ first-look/bootstrap.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-mvn io.quarkus:quarkus-maven-plugin:3.0.0.Alpha1:create \
-    -DplatformVersion=3.0.0.Alpha1 \
+mvn io.quarkus:quarkus-maven-plugin:3.0.0.Alpha2:create \
+    -DplatformVersion=3.0.0.Alpha2 \
     -DprojectGroupId=org.agoncal.fascicle.quarkus \
-    -DprojectArtifactId=first-step \
+    -DprojectArtifactId=first-look \
     -DprojectVersion=3.0.0-SNAPSHOT \
     -DclassName="org.agoncal.fascicle.quarkus.firststep.AuthorResource" \
     -Dpath="authors"
