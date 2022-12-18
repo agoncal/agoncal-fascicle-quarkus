f20698d31dcecf0630c3571542cec4a98e26b1db
Antonio Goncalves <antonio.goncalves@gmail.com>
2022-12-18 21:35:46 +0100

    Bump Quarkus Alpha2

--- getting-started/bootstrap-maven.sh
+++ getting-started/bootstrap-maven.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # tag::adocSnippet[]
-mvn io.quarkus:quarkus-maven-plugin:3.0.0.Alpha1:create \
+mvn io.quarkus:quarkus-maven-plugin:3.0.0.Alpha2:create \
     -DplatformVersion=3.0.0.Alpha2 \
     -DprojectGroupId=org.agoncal.fascicle.quarkus \
     -DprojectArtifactId=getting-started \
--- putting-together/rest-book/bootstrap.sh
+++ putting-together/rest-book/bootstrap.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # tag::adocSnippet[]
-mvn io.quarkus:quarkus-maven-plugin:3.0.0.Alpha1:create \
-    -DplatformVersion=3.0.0.Alpha1 \
+mvn io.quarkus:quarkus-maven-plugin:3.0.0.Alpha2:create \
+    -DplatformVersion=3.0.0.Alpha2 \
     -DprojectGroupId=org.agoncal.fascicle.quarkus.putting-together \
     -DprojectArtifactId=rest-book \
     -DprojectVersion=3.0.0-SNAPSHOT \
