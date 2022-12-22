b87e9f6cd91ba86e9a6f635a61ef86d67edd55e2
Antonio Goncalves <antonio.goncalves@gmail.com>
2022-12-22 16:14:48 +0000

    Simplified Log

--- core/logging/logging-loggers/src/main/java/org/agoncal/fascicle/quarkus/core/logging/SimplifiedLoggingResource.java
+++ core/logging/logging-loggers/src/main/java/org/agoncal/fascicle/quarkus/core/logging/SimplifiedLoggingResource.java
@@ -1,16 +1,19 @@
 package org.agoncal.fascicle.quarkus.core.logging;
 
+// tag::adocSnippet[]
 import io.quarkus.logging.Log;
+
+// end::adocSnippet[]
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 
+// tag::adocSnippet[]
 @Path("/logs/simple")
 public class SimplifiedLoggingResource {
 
-  // tag::adocSnippet[]
   @GET
   public void displayLogs() {
     Log.info("Trace produced by Simplified Logging API");
   }
-  // end::adocSnippet[]
 }
+// end::adocSnippet[]
