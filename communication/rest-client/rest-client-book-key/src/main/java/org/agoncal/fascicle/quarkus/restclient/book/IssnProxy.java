c8e4eef2ee398a7a7cbd00a65b40f98f25715fe3
Antonio Goncalves <antonio.goncalves@gmail.com>
2022-12-28 17:28:05 +0100

    Config Key

---
+++ communication/rest-client/rest-client-book-key/src/main/java/org/agoncal/fascicle/quarkus/restclient/book/IssnProxy.java
@@ -0,0 +1,20 @@
+package org.agoncal.fascicle.quarkus.restclient.book;
+
+import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+
+import jakarta.json.JsonObject;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+// tag::adocSnippet[]
+@Path("/api/issn")
+@Produces(MediaType.APPLICATION_JSON)
+@RegisterRestClient(configKey = "issnProxy")
+public interface IssnProxy {
+
+  @GET
+  JsonObject generateIssn();
+}
+// end::adocSnippet[]
