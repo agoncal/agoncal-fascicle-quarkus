d612f05d6c8245fc4d6abc4ec5b53b8a3ee0fa4e
Antonio Goncalves <antonio.goncalves@gmail.com>
2023-01-08 14:24:26 +0100

    Look

--- first-look/bootstrap.sh
+++ first-look/bootstrap.sh
@@ -4,6 +4,6 @@ mvn io.quarkus:quarkus-maven-plugin:3.0.0.Alpha2:create \
     -DprojectGroupId=org.agoncal.fascicle.quarkus \
     -DprojectArtifactId=first-look \
     -DprojectVersion=3.0.0-SNAPSHOT \
-    -DclassName="org.agoncal.fascicle.quarkus.firststep.AuthorResource" \
+    -DclassName="org.agoncal.fascicle.quarkus.firstlook.AuthorResource" \
     -Dpath="authors"
 
