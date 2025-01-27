0846e6599bdcc4b88dc963b9230ceec55068ee61
Antonio Goncalves <antonio.goncalves@gmail.com>
2025-01-27 09:41:35 +0100

    Update

--- first-steps-getting-started/bootstrap-quarkuscli.sh
+++ first-steps-getting-started/bootstrap-quarkuscli.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 # tag::adocSnippet[]
 quarkus create app org.agoncal.fascicle.quarkus:getting-started:3.0.0-SNAPSHOT \
-    --stream io.quarkus.platform:3.17.6 \
     --name "Artist" \
     --package-name "org.agoncal.fascicle.quarkus.gettingstarted" \
     --extensions="resteasy-jsonb" \
