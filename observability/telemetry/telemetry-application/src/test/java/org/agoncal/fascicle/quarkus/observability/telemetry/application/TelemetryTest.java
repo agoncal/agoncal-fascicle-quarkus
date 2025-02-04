848d855631c9119377f5c04ad0230ab01131d487
Antonio Goncalves <antonio.goncalves@gmail.com>
2025-02-04 12:57:01 +0100

    Telemetry

---
+++ observability/telemetry/telemetry-application/src/test/java/org/agoncal/fascicle/quarkus/observability/telemetry/application/TelemetryTest.java
@@ -0,0 +1,54 @@
+package org.agoncal.fascicle.quarkus.observability.telemetry.application;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static jakarta.ws.rs.core.HttpHeaders.ACCEPT;
+import static jakarta.ws.rs.core.MediaType.APPLICATION_JSON;
+import static jakarta.ws.rs.core.Response.Status.OK;
+
+//@formatter:off
+@QuarkusTest
+public class TelemetryTest {
+
+  @Test
+  void shouldPingMetrics() {
+    given()
+      .header(ACCEPT, APPLICATION_JSON).
+    when()
+      .get("/q/metrics").
+    then()
+      .statusCode(OK.getStatusCode());
+  }
+
+  @Test
+  void shouldPingMetricsApplication() {
+    given()
+      .header(ACCEPT, APPLICATION_JSON).
+    when()
+      .get("/q/metrics/application").
+    then()
+      .statusCode(OK.getStatusCode());
+  }
+
+  @Test
+  void shouldPingMetricsBase() {
+    given()
+      .header(ACCEPT, APPLICATION_JSON).
+    when()
+      .get("/q/metrics/base").
+    then()
+      .statusCode(OK.getStatusCode());
+  }
+
+  @Test
+  void shouldPingMetricsVendor() {
+    given()
+      .header(ACCEPT, APPLICATION_JSON).
+    when()
+      .get("/q/metrics/vendor").
+    then()
+      .statusCode(OK.getStatusCode());
+  }
+}
